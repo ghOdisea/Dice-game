9874c24f88085684ebb203d97b6f5b4143ce4662
ghOdisea <154799410+ghOdisea@users.noreply.github.com>
2024-06-10 11:25:02 +0200

    docker & env update

--- src/infrastructure/database/mySQL/config.sequelize.ts
+++ src/infrastructure/database/mySQL/config.sequelize.ts
@@ -7,25 +7,27 @@ const MYSQL_DOCKER_HOST = String(process.env.MYSQL_DOCKER_HOST) || ''
 const MYSQL_USER = process.env.MYSQL_USER?.trim()
 const MYSQL_DATABASE = process.env.MYSQL_DATABASE || ''
 const MYSQL_PASSWORD = String(process.env.MYSQL_PASSWORD) || ''
-const MYSQL_DOCKER_PORT = Number(process.env.MYSQL_DOCKER_PORT)
+// const MYSQL_DOCKER_PORT = Number(process.env.MYSQL_DOCKER_PORT)
+const MYSQL_LOCAL_PORT = Number(process.env.MYSQL_LOCAL_PORT)
 
-console.log({
-  host: MYSQL_DOCKER_HOST,// 'localhost'
-  user: MYSQL_USER,
-  password: MYSQL_PASSWORD,
-  db: MYSQL_DATABASE
-})
 
 if (!MYSQL_DOCKER_HOST || !MYSQL_USER || !MYSQL_DATABASE || !MYSQL_PASSWORD) {
   throw new Error('One or more required environment variables are missing')
 }
+  
+console.log({
+  // host_Docker: MYSQL_DOCKER_HOST,// 'localhost'
+  user: MYSQL_USER,
+  // password: MYSQL_PASSWORD,
+  db: MYSQL_DATABASE,
+  db_local_port: MYSQL_LOCAL_PORT,
+  // db_docker_port: MYSQL_DOCKER_PORT,
+})
 
-
-export const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, {  //'123456'
-  host: MYSQL_DOCKER_HOST, // 'localhost'
-  dialect: 'mysql',
-  port: MYSQL_DOCKER_PORT,
-  define: {
-    timestamps: false,
-  },
+export const sequelize = new Sequelize('mysql://localhost:3307/rolling_dices', {
+  username: MYSQL_USER,
+  password: MYSQL_PASSWORD,
+  define:{
+    timestamps: false
+  }
 })
